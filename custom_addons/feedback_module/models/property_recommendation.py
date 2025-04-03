033f54b660b18cb0c2ecd7efc68e3a1b92d86cad
abdelrahman samir <abdelrahman.200300@gmail.com>
2025-04-03 19:47:47 +0200

    development mode buf fix

--- custom_addons/feedback_module/models/property_recommendation.py
+++ custom_addons/feedback_module/models/property_recommendation.py
@@ -1,20 +1,21 @@
 from odoo import models, fields, api
 
 class PropertyRecommendation(models.Model):
-        _name = 'property.recommendation'
-        _description = 'Property Recommendation'
+    _name = 'property.recommendation'
+    _description = 'Property Recommendation'
+    _order = 'recommendation_date desc'
 
-        name = fields.Char(string='Recommendation Title', required=True)
-        id = fields.Char(string='ID', readonly=True, copy=False, default='New')
-        
-        customer_id = fields.Many2one( string='Customer', required=True)
-        customer_name = fields.Char( string='Customer Name', store=True)
-        
-        recommendation_date = fields.Datetime(string='Recommendation Date', default=fields.Datetime.now)
-        feedback = fields.Text(string='Customer Feedback', required=True)
-        
-        # recommended_property_id = fields.Many2one('crm.lead', string='Recommended Property', required=False)
-        recommended_property_id = fields.Char(string='Recommended Property')
+    # Core fields
+    name = fields.Char(string='Recommendation Title', required=True, index=True)
+    recommendation_date = fields.Datetime(string='Recommendation Date', default=fields.Datetime.now, index=True)
+    user_id = fields.Many2one('res.users', string='User', required=True, index=True)
+    
+    # Property related fields
+    recommended_property_id = fields.Char(string='Recommended Property', index=True)
+    feedback_property_id = fields.Char(string='Feedback Property ID', required=True, index=True)
+    feedback_property_name = fields.Char(string='Property Name', required=True, index=True)
+    
+    # Feedback
+    feedback = fields.Text(string='Feedback', required=True)
 
-        feedback_property_id = fields.Char(string='Feedback Property ID', required=True)
-        feedback_property_name = fields.Char(string='Property Name', required=True)
+            
